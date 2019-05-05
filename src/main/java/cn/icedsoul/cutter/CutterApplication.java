7a6c257e03fda05b3afbfaca9bb5015881b6bc64
IcedSoul <1443700905@qq.com>
2019-05-05 10:03:17 +0800

    init project

---
+++ src/main/java/cn/icedsoul/cutter/CutterApplication.java
@@ -0,0 +1,13 @@
+package cn.icedsoul.cutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CutterApplication.class, args);
+    }
+
+}
