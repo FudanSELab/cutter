bc86827442cad70509365f2862533ba219c2128f
IcedSoul <1443700905@qq.com>
2019-05-16 09:50:28 +0800

    add handle data api and swagger

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM java:8-jre
 
 ADD ./target/cutter-0.0.1-SNAPSHOT.jar /app/
+ADD data/kieker-20190514-075832442-UTC-001.dat /app/
 CMD ["java", "-Xmx400m", "-jar", "/app/cutter-0.0.1-SNAPSHOT.jar"]
 
 EXPOSE 16318
--- src/main/java/cn/icedsoul/cutter/controller/CutterController.java
+++ src/main/java/cn/icedsoul/cutter/controller/CutterController.java
@@ -1,26 +1,49 @@
 package cn.icedsoul.cutter.controller;
 
+import cn.icedsoul.cutter.service.api.HandleDataService;
 import cn.icedsoul.cutter.service.api.TableCutService;
 import cn.icedsoul.cutter.service.api.WeightCalculationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
+
+/**
+ * @author icedsoul
+ */
 @RestController
+@Api(value = "controller of cutter")
 public class CutterController {
+
+    @Autowired
+    HandleDataService handleDataService;
+
     @Autowired
     WeightCalculationService weightCalculationService;
+
     @Autowired
     TableCutService tableCutService;
 
     @CrossOrigin(origins = "*")
-    @RequestMapping(value = "/addweight", method ={RequestMethod.GET})
-    public void addweight(){
+    @PostMapping(value = "/data")
+    @ApiOperation(value = "Read dat file", notes = "Add nodes and relations")
+    public void handleData(@PathParam("file")String file){
+        handleDataService.handleData(file);
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(value = "/weight")
+    @ApiOperation(value = "Add weight", notes = "Add weight")
+    public void addWeight(){
         weightCalculationService.addWeight();
     }
 
     @CrossOrigin(origins = "*")
-    @RequestMapping(value = "/cuttable", method ={RequestMethod.GET})
-    public void cutTable(@RequestParam(required = true) int k){
+    @PostMapping(value = "/cut")
+    @ApiOperation(value = "Cut table", notes = "Cut table to k parts")
+    public void cutTable(@PathParam("k") int k){
         tableCutService.cutTable(k);
     }
 
